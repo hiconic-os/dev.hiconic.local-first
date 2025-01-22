bbcdbdda415401d30c17b5e1bb8f03228129804d
tryptamic <dirk.scheffler@modularmind.eu>
2025-01-22 12:17:44 +0100

    ADW-118: special handling for signing withdraw

---
+++ local-first-js/test/draft.test.ts
@@ -0,0 +1,10 @@
+//import "../src/symbol-test"
+import { describe, it, expect } from "vitest";
+import { Resource } from "@dev.hiconic/gm_resource-model"
+import { TransientSource } from "@dev.hiconic/gm_transient-resource-model"
+import { generateReplication } from "./replication-helper.js"
+
+describe("draft tests", () => {
+  it("draft", async () => {
+  });
+});
